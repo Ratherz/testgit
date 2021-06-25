3c85ce5188441aa1b330561ebf7b48b411c5310c
Sopon Ngernsawang <ply159159@gmail.com>
2021-06-25 09:15:53 +0700

    test3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Hello");
-<<<<<<< HEAD
+console.log("Hola");
 
 let number1 = 10;
 let number2 = 20;
@@ -7,6 +7,4 @@ console.log(number1+number2);
 let arr = [];
 arr.push(number1);
 arr.push(number2);
-=======
-console.log("Hola");
->>>>>>> 39ab0fba93de0a2ca4ca2aeba1a2bc8708c1bc17
+
