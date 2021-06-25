664975f0cc5f2d2efc6f035eb5e822e2b68797e9
Chayanonjackal <aom_jackal@hotmail.com>
2021-06-25 09:43:27 +0700

    hiii logger

---
+++ loger.js
@@ -0,0 +1,9 @@
+console.log("This is logger")
+console.log("This is logger")
+console.log("This is logger")
+console.log("This is logger")
+console.log("This is logger")
+console.log("This is logger")
+console.log("This is logger")
+
+const f = 50 ;
